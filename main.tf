147cb1082b744acb3c2701b66ad1093104e89f00
Pradyuman <pradyuman@Pradyumans-Mac-mini.local>
2025-07-21 16:32:35 +0530

    update jenkins

--- main.tf
+++ main.tf
@@ -8,9 +8,27 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
-  
+  region = "us-east-1" # Change this to your region
 }
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical (Ubuntu publisher)
+}
+
+
+
 
 resource "aws_instance" "web" {
   count = 4
@@ -22,6 +40,7 @@ resource "aws_instance" "web" {
   }
 }
 
+/*
 resource "aws_security_group" "sg" {
   count = 2
   name = "web_sg"
@@ -47,3 +66,4 @@ resource "aws_security_group" "sg" {
   }
   
 }
+*/
