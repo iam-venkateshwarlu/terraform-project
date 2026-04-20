27a847e214dbf3c5f13f396aa7653ced47579f2e
iam-venkateshwarlu <tvenkateshwarlu39@gmail.com>
2026-04-20 18:03:30 +0530

    ec2 updated

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "this" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  key_name      = var.key_name
-  subnet_id     = var.subnet_id
-  vpc_security_group_ids = [var.security_group_id]
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  subnet_id                   = var.subnet_id
+  vpc_security_group_ids      = [var.security_group_id]
+  key_name                    = var.key_name
   associate_public_ip_address = true
 
   tags = {
--- variable.tf
+++ variable.tf
@@ -1,44 +1,44 @@
 variable "aws_region" {
-    description = "The AWS region to deploy resources"
-    type        = string
-    default     = "ap-south-1"
+  description = "The AWS region to deploy resources"
+  type        = string
+  default     = "ap-south-1"
 }
 
 variable "project_name" {
-    description = "The name of the project"
-    type        = string
-    default     = "my-terraform-project"
+  description = "The name of the project"
+  type        = string
+  default     = "my-terraform-project"
 }
 
 variable "vpc_cidr" {
-    description = "The CIDR block for the VPC"
-    type        = string
-    default     = "10.0.0.0/16"
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidr" {
-    description = "The CIDR block for the public subnet"
-    type        = string
-    default     = "10.0.1.0/24"
+  description = "The CIDR block for the public subnet"
+  type        = string
+  default     = "10.0.1.0/24"
 }
 
 variable "ami_id" {
-    description = "Amazon Linux 2 or Ubuntu AMI"
-    type        = string
+  description = "Amazon Linux 2 or Ubuntu AMI"
+  type        = string
 }
 
 variable "my_ip" {
-    description = "The public IP address of the user"
-    type        = string
+  description = "The public IP address of the user"
+  type        = string
 }
 
 variable "instance_type" {
-    description = "The EC2 instance type"
-    type        = string
-    default     = "t2.micro"
+  description = "The EC2 instance type"
+  type        = string
+  default     = "t2.micro"
 }
 
 variable "key_name" {
-    description = "Existing EC2 key pair name"
-    type        = string
+  description = "Existing EC2 key pair name"
+  type        = string
 }
